75ab8623910df0350a67b2136db72753feb575ae
Qianna <892878159@qq.com>
2021-10-20 15:26:03 +0800

    update

---
+++ configs/tmp/tmp_msmt17.py
@@ -0,0 +1,98 @@
+_base_ = '../_base_/default_runtime.py'
+
+model = dict(
+    type='TMP',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        out_indices=[3],
+        strides=(1, 2, 2, 1),
+        norm_cfg=dict(type='BN'),
+        norm_eval=False),
+    neck=dict(
+        type='BNNeck',
+        feat_dim=2048,
+        norm_cfg=dict(type='BN1d'),
+        with_bias=False,
+        with_avg_pool=True,
+        avgpool=dict(type='AvgPoolNeck')),
+    head=dict(type='AnotherSCLHead', temperature=0.05))
+
+data_source = dict(type='MSMT17', data_root='/root/data/zq/data/MSMT17')
+dataset_type = 'ContrastiveDataset'
+train_pipeline = [
+    dict(
+        type='RandomCamStyle',
+        camstyle_root='bounding_box_train_camstyle',
+        p=0.5),
+    dict(type='LoadImage'),
+    dict(
+        type='RandomResizedCrop',
+        size=(256, 128),
+        scale=(0.64, 1.0),
+        ratio=(0.33, 0.5),
+        interpolation=3),
+    dict(type='RandomHorizontalFlip'),
+    dict(type='RandomRotation', degrees=10),
+    dict(
+        type='RandomApply',
+        transforms=[dict(type='GaussianBlur', sigma=(0.1, 2.0))],
+        p=0.5),
+    dict(type='ToTensor'),
+    dict(
+        type='Normalize',
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]),
+    dict(type='RandomErasing', value=[0.485, 0.456, 0.406])
+]
+test_pipeline = [
+    dict(type='LoadImage'),
+    dict(type='Resize', size=(256, 128), interpolation=3),
+    dict(type='ToTensor'),
+    dict(
+        type='Normalize',
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
+]
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=4,
+    sampler=dict(
+        type='FixedStepIdentitySampler',
+        num_instances=4,
+        step=50,
+        with_camid=True),
+    train=dict(
+        type=dataset_type, data_source=data_source, pipeline=train_pipeline),
+    test=dict(
+        type='ReIDDataset',
+        data_source=data_source,
+        pipeline=test_pipeline,
+        test_mode=True))
+
+custom_hooks = [
+    dict(
+        type='LabelGenerationHook',
+        extractor=dict(
+            dataset=dict(
+                type='ReIDDataset',
+                data_source=data_source,
+                pipeline=test_pipeline),
+            samples_per_gpu=32,
+            workers_per_gpu=4),
+        label_generator=dict(
+            type='SelfPacedGenerator', eps=[0.75], min_samples=4, k1=30, k2=6),
+        interval=1)
+]
+# optimizer
+optimizer = dict(type='SGD', lr=0.1, weight_decay=0.0001, momentum=0.9)
+# learning policy
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=0.,
+    warmup='linear',
+    warmup_iters=5,
+    warmup_ratio=0.0001,  # cannot be 0
+    warmup_by_epoch=True)
+total_epochs = 200
