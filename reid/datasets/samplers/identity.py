2b856f23b9e9406f5b176caa931651f47cbc330a
Qianna <892878159@qq.com>
2021-10-27 17:40:08 +0800

    update

--- reid/datasets/samplers/identity.py
+++ reid/datasets/samplers/identity.py
@@ -130,7 +130,6 @@ class DistributedFixedStepIdentitySampler(FixedStepIdentitySampler):
         if rank is None:
             rank = _rank
         self.num_replicas = num_replicas
-        print("num_replicas:", self.num_replicas)
         self.rank = rank
         # batch_size of single process -> total batch size
         batch_size = batch_size * self.num_replicas
