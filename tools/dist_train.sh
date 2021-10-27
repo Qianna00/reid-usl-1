cfae2a7c00ea9f715c819d5fa054eee73806b3df
Qianna <892878159@qq.com>
2021-10-27 17:34:31 +0800

    update FixedStepIdentitySampler init_data

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -2,8 +2,8 @@
 
 CONFIG=$1
 GPUS=$2
-PORT=${PORT:-29520}
+PORT=${PORT:-29517}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --sync-bn --deterministic --work-dir /root/vsislab-2/zq/ICCV_hex/tmp/msmt17/try --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --sync-bn --deterministic --work-dir /root/vsislab-2/zq/ICCV_hex/temp --launcher pytorch ${@:3}
