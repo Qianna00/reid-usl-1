498fda80194f34ee88d4ee33adf8a1dc24da5302
Qianna00 <41325698+Qianna00@users.noreply.github.com>
2021-11-07 20:06:35 +0800

    Update tmp.py

--- reid/models/reids/tmp.py
+++ reid/models/reids/tmp.py
@@ -9,7 +9,7 @@ from .baseline import Baseline
 @REIDS.register_module()
 class TMP(Baseline):
 
-    def forward_train(self, img, **kwargs):
+    def forward_train(self, img, label, camid, label_cam, **kwargs):
         assert img.dim() == 5, f'img must be 5 dims, but got: {img.dim()}'
         batch_size = img.shape[0]
 
@@ -18,8 +18,17 @@ class TMP(Baseline):
         z = F.normalize(z, dim=1)
 
         z1, z2 = torch.split(z, [batch_size, batch_size], dim=0)
-        z = torch.cat((z1.unsqueeze(1), z2.unsqueeze(1)), dim=1)
+        feats_list = []
+        label_camid_list = []
+        cam_ids = torch.unique(camid).tolist()
+        for cam_id in cam_ids:
+            index = torch.nonzero(camid == cam_id, as_tuple=False).view(-1)
+            feats = torch.cat((z1[index].unsqueeze(1), z2[index].unsqueeze(1)), dim=1)
+            label_cam_id = label_cam[index]
+            feats_list.append(torch.cat(GatherLayer.apply(feats), dim=0)
+            label_camid_list.append(label_cam_id)
+        z = torch.cat((z1.unsqueeze(1), z2.unsqueeze(1)), dim=1)  
         z = torch.cat(GatherLayer.apply(z), dim=0)
-        losses = self.head(z, **kwargs)
+        losses = self.head(z, label, feats_list, label_camid_list, **kwargs)
 
         return losses
