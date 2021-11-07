9dbb663d5707911b678e2bf3c208e156c436f633
Qianna00 <41325698+Qianna00@users.noreply.github.com>
2021-11-07 20:15:10 +0800

    Update tmp.py

--- reid/models/reids/tmp.py
+++ reid/models/reids/tmp.py
@@ -25,7 +25,7 @@ class TMP(Baseline):
             index = torch.nonzero(camid == cam_id, as_tuple=False).view(-1)
             feats = torch.cat((z1[index].unsqueeze(1), z2[index].unsqueeze(1)), dim=1)
             label_cam_id = label_cam[index]
-            feats_list.append(torch.cat(GatherLayer.apply(feats), dim=0)
+            feats_list.append(torch.cat(GatherLayer.apply(feats), dim=0))
             label_camid_list.append(label_cam_id)
         z = torch.cat((z1.unsqueeze(1), z2.unsqueeze(1)), dim=1)  
         z = torch.cat(GatherLayer.apply(z), dim=0)
