bd956bab5ff0f8ed6a92a946520e00b7914add1a
Qianna <892878159@qq.com>
2021-11-23 17:01:05 +0800

    update

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,4 +6,6 @@ PORT=${PORT:-29517}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --sync-bn --deterministic --work-dir /root/vsislab-2/zq/ICCV_hex/spcl/tmp-pretrain --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --sync-bn --deterministic \
+    --resume-from /root/vsislab-2/zq/ICCV_hex/tmp/no_pretrain/latest.pth \
+    --work-dir /root/vsislab-2/zq/ICCV_hex/tmp/no_pretrain --launcher pytorch ${@:3}
